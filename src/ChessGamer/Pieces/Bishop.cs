bda98d2eca9c61f1a371623136cfe583a2a22d82
klinger <klinger@klingersystem.com.br>
2024-05-07 22:03:18 -0300

    Tower

--- src/ChessGamer/Pieces/Bishop.cs
+++ src/ChessGamer/Pieces/Bishop.cs
@@ -5,7 +5,12 @@
         public Bishop(ConsoleColor color) : base(color) {
         }
         
-        internal override bool ValidFilds(int line, int column, ref Field[,] fields) {
+        internal override bool ValidFilds(int row, int column, ref Field[,] fields) {
+            for (int rrow = 0; rrow < 8; rrow++) {
+                for (int ccolumn = 0; ccolumn < 8; ccolumn++) {
+
+                }
+            }
             return true;
         }
 
--- src/ChessGamer/Pieces/Tower.cs
+++ src/ChessGamer/Pieces/Tower.cs
@@ -4,9 +4,49 @@
     public class Tower : Piece {
         public Tower(ConsoleColor color) : base(color) {
         }
-                
+
         internal override bool ValidFilds(int row, int column, ref Field[,] fields) {
+            var current = fields[row, column];
+
+            for (int i = row - 1; i >= 0; i--) {
+                if (VerifildPosition(i, column, current, ref fields)) {
+                    break;
+                }
+            }
+
+            for (int i = row + 1; i < 8; i++) {
+                if (VerifildPosition(i, column, current, ref fields)) {
+                    break;
+                }
+            }
+
+            for (int i = column - 1; i >= 0; i--) {
+                if (VerifildPosition(row, i, current, ref fields)) {
+                    break;
+                }
+            }
+
+            for (int i = column + 1; i < 8; i++) {
+                if (VerifildPosition(row, i, current, ref fields)) {
+                    break;
+                }
+            }
             return true;
+        }
+
+        private bool VerifildPosition(int row, int collum, Field current, ref Field[,] fields) {
+            var fieldd = fields[row, collum];
+            if (fieldd.Piece != null) {
+                if (fieldd.Piece.Color != current.Piece.Color) {
+                    fieldd.EnableValidPosition();
+                    return true;
+                } else {
+                    return true;
+                }
+            } else {
+                fieldd.EnableValidPosition();
+            }
+            return false;
         }
 
         public override string ToString() {
